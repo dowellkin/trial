4dc50191ad0f1df7e6bdc32791822c9ecf7c6e99
Alexandr Korolev <dowellkin@gmail.com>
2022-02-15 23:08:19 +0300

    add clipboard btn

--- src/views/Rates.vue
+++ src/views/Rates.vue
@@ -20,6 +20,14 @@
 						</router-link>
 					</template>
 
+					<template v-slot:[`item.actions`]="{ item }">
+						<v-btn @click="copy(item)">
+							<v-icon>
+								{{ copyed == item.Cur_Abbreviation ? 'mdi-check' : 'mdi-clipboard'}}
+							</v-icon>
+						</v-btn>
+					</template>
+
 				</v-data-table>
 			</v-col>
 		</v-row>
@@ -36,8 +44,10 @@ export default {
 			{ text: 'id', value: 'Cur_ID' },
 			{ text: 'Аббревиатура', value: 'Cur_Abbreviation' },
 			{ text: 'Курс', value: 'Cur_OfficialRate' },
+			{ text: 'Скопировать', value: 'actions' },
 		],
 		selected: [],
+		copyed: null
 	}),
 	computed: {
 		...mapGetters('settings', ['interestedCurrencies']),
@@ -46,6 +56,18 @@ export default {
 		}
 	},
 	methods: {
+		copy(data){
+			console.log(data);
+			const date = new Date(data.Date);
+			const day = date.getDate();
+			const month = date.getMonth() +1;
+			const year = date.getFullYear();
+			const str = `${data.Cur_Name} ${data.Cur_OfficialRate.toFixed(2)}р на ${String(day).padStart(2, "0")}.${String(month).padStart(2, "0")}.${year}`;
+			navigator.clipboard.writeText(str)
+			.then(() => {
+				this.copyed = data.Cur_Abbreviation
+			})
+		}
 	}
 }
 </script>
